94207e963ea941c6d8f6be355b1532ae2744dc34
xumengze <13240327001@163.com>
2023-04-08 21:09:06 +0800

    lc

---
+++ src/com/ita/alg/dp/StockExchange.java
@@ -0,0 +1,71 @@
+package com.ita.alg.dp;
+
+import java.util.Arrays;
+
+public class StockExchange {
+    /**
+     * 假设你有一个数组prices，长度为n，其中prices[i]是股票在第i天的价格，请根据这个价格数组，返回买卖股票能获得的最大收益
+     * 1.你可以买入一次股票和卖出一次股票，并非每天都可以买入或卖出一次，总共只能买入和卖出一次，且买入必须在卖出的前面的某一天
+     * 2.如果不能获取到任何利润，请返回0
+     * 3.假设买入卖出均无手续费
+     */
+    public int maxProfit1 (int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+        int[][] dp = new int[len][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+
+
+    /**
+     * 假设你有一个数组prices，长度为n，其中prices[i]是某只股票在第i天的价格，请根据这个价格数组，返回买卖股票能获得的最大收益
+     * 1. 你可以多次买卖该只股票，但是再次购买前必须卖出之前的股票
+     * 2. 如果不能获取收益，请返回0
+     * 3. 假设买入卖出均无手续费
+     */
+    public int maxProfit2 (int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+        int[][] dp = new int[len][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < len; i++){
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+
+
+    /**
+     * 假设你有一个数组prices，长度为n，其中prices[i]是某只股票在第i天的价格，请根据这个价格数组，返回买卖股票能获得的最大收益
+     * 1. 你最多可以对该股票有两笔交易操作，一笔交易代表着一次买入与一次卖出，但是再次购买前必须卖出之前的股票
+     * 2. 如果不能获取收益，请返回0
+     * 3. 假设买入卖出均无手续费
+     */
+    public int maxProfit3 (int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][5];
+        Arrays.fill(dp[0], -1);
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        return Math.max(dp[n - 1][2],Math.max(0, dp[n - 1][4]));
+    }
+}
